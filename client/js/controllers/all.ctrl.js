d772eb677409952c93b7b7216df92d7687a0c43c
joseph park <jpark041@gmail.com>
2016-01-28 11:32:16 -0800

    repush

--- client/js/controllers/all.ctrl.js
+++ client/js/controllers/all.ctrl.js
@@ -64,32 +64,6 @@ myAngularObject.controller("DashboardController", function(UserFactory, ParkFact
     var directions = new google.maps.DirectionsRoute();
     var distance = new google.maps.DistanceMatrixRequest();
 
-		if (navigator.geolocation) {
-	    navigator.geolocation.getCurrentPosition(function(position) {
-	      var pos = {
-	        lat: position.coords.latitude,
-	        lng: position.coords.longitude
-	      };
-
-	      infoWindow.setPosition(pos);
-	      infoWindow.setContent('Location found.');
-	      map.setCenter(pos);
-	    }, function() {
-	      handleLocationError(true, infoWindow, map.getCenter());
-	    });
-	  } else {
-	    // Browser doesn't support Geolocation
-	    handleLocationError(false, infoWindow, map.getCenter());
-	  }
-	}
-
-	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-	  infoWindow.setPosition(pos);
-	  infoWindow.setContent(browserHasGeolocation ?
-	                        'Error: The Geolocation service failed.' :
-	                        'Error: Your browser doesn\'t support geolocation.');
-		}
-	});
 
 	service.nearbySearch(request, callback);
 	// UserFactory.loggedUser(function(user){
