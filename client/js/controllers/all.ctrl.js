d722132128067beae8634dfbaf02d0d202bfae3e
joseph park <jpark041@gmail.com>
2016-01-29 01:03:59 -0800

    maps loaded, need to be organized

--- client/js/controllers/all.ctrl.js
+++ client/js/controllers/all.ctrl.js
@@ -89,35 +89,42 @@ myAngularObject.controller("DashboardController", function(UserFactory, ParkFact
 			_this.users = users
 		})
 	}
- //    function performSearch() {
- //  	var request = {
- //    bounds: map.getBounds(),
- //    keyword: 'basketball court'
- //  	};
+	function initialize() {
+	  var initposition = new google.maps.LatLng(dashCtrl.position.lat, dashCtrl.position.lng);
+	  console.log(initposition);
+	  var map = new google.maps.Map(document.getElementById('map'), {
+	    center: initposition,
+	    zoom: 15,
+	    scrollwheel: false
+	  });
 
- //  	service.radarSearch(request, callback);
-	// }
+	  // Specify location, radius and place types for your Places API search.
+	  var request = {
+	    location: initposition,
+	    radius: '3500',
+	    types: ['basketball']
+	  };
 
-	// function callback(results, status) {
-	//   if (status !== google.maps.places.PlacesServiceStatus.OK) {
-	//     console.error(status);
-	//     return;
-	//   }
-	//   for (var i = 0, result; result = results[i]; i++) {
-	//     addMarker(result);
-	//   }
-	// }
-
-	// function addMarker(place) {
-	//   var marker = new google.maps.Marker({
-	//   	map: map,
-	//     position: place.geometry.location,
-	//     icon: {
-	//       url: 'http://maps.gstatic.com/mapfiles/circle.png',
-	//       anchor: new google.maps.Point(10, 10),
-	//       scaledSize: new google.maps.Size(10, 17)
-	//     }
-	//   });
+	  // Create the PlaceService and send the request.
+	  // Handle the callback with an anonymous function.
+	  var service = new google.maps.places.PlacesService(map);
+	  service.nearbySearch(request, function(results, status) {
+	    if (status == google.maps.places.PlacesServiceStatus.OK) {
+	      for (var i = 0; i < results.length; i++) {
+	        var place = results[i];
+	        // If the request succeeds, draw the place location on
+	        // the map as a marker, and register an event to handle a
+	        // click on the marker.
+	        var marker = new google.maps.Marker({
+	          map: map,
+	          position: place.geometry.location
+	        });
+	      }
+	    }
+	  });
+	}
+	// Run the initialize function when the window has finished loading.
+	google.maps.event.addDomListener(window, 'load', initialize);
 
 	//   google.maps.event.addListener(marker, 'click', function() {
 	//     service.getDetails(place, function(result, status) {
--- client/js/lib/map.js
+++ client/js/lib/map.js
@@ -1,33 +1,134 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
-  });
-  var infoWindow = new google.maps.InfoWindow({map: map});
+var infowindow;
+var map;
+     function success(position)
+     {
+           // Define the coordinates as a Google Maps LatLng Object
+           var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
+           // Prepare the map options
+           var mapOptions =
+          {
+                      zoom: 14,
+                      center: coords,
+                      mapTypeControl: false,
+                      navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+                      mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            // Create the map, and place it in the map_canvas div
+            map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+      
+      //search for schools within 1500 metres of our current location, and as a marker use school.png
+      placesRequest('Schools',coords,1500,['school'],'images/school.png');
+
+
+            // Place the initial marker
+            var marker = new google.maps.Marker({
+                      position: coords,
+                      map: map,
+                      title: "Your current location!"
+            });
+      
+        }
+
+        function fail()
+        {
+              // Could not obtain location
+        }
+    
+    //Request places from Google
+    function placesRequest(title,latlng,radius,types,icon)
+    {
+      //Parameters for our places request
+      var request = {
+        location: latlng,
+        radius: radius,
+        types: types
       };
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-}
+      //Make the service call to google
+      var callPlaces = new google.maps.places.PlacesService(map);
+      callPlaces.search(request, function(results,status){
+        //trace what Google gives us back
+        $.each(results, function(i,place){
+          var placeLoc = place.geometry.location;
+           var thisplace = new google.maps.Marker({
+             map: map,
+             position: place.geometry.location,
+             icon: icon,
+             title: place.name
+           });
+        })
+      });
+    }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+// function createMap(coords, zoom){
+//     var mapOptions = {
+//         zoom: zoom,
+//         center: new google.maps.LatLng(coords.latitude, coords.longitude),
+//         panControl: false,
+//         panControlOptions: {
+//             coords: google.maps.ControlPosition.BOTTOM_LEFT
+//         },
+//         zoomControl: true,
+//         zoomControlOptions: {
+//             style: google.maps.ZoomControlStyle.LARGE,
+//             coords: google.maps.ControlPosition.RIGHT_CENTER
+//         },
+//         scaleControl: false
+//     };
+//     infowindow = new google.maps.InfoWindow({
+//         content: "holding..."
+//     });
+//     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+// }
+
+// function initMap() {
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: dashCtrl.position.lat, lng: dashCtrl.position.lng},
+//     zoom: 6
+//   });
+
+//   var infoWindow = new google.maps.InfoWindow({map: map});
+
+//     function performSearch() {
+//     var request = {
+//     bounds: map.getBounds(),
+//     keyword: 'basketball court'
+//     };
+
+//     service.radarSearch(request, callback);
+//   }
+
+  function callback(results, status) {
+    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+      console.error(status);
+      return;
+    }
+    for (var i = 0, result; result = results[i]; i++) {
+      addMarker(result);
+    }
+  }
+
+  function geocodePlaceId(geocoder, map, infowindow) {
+    var placeId = document.getElementById('place-id').value;
+    geocoder.geocode({'placeId': placeId}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          map.setZoom(11);
+          map.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+          });
+          infowindow.setContent(results[0].formatted_address);
+          infowindow.open(map, marker);
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    })
+  }
 }
