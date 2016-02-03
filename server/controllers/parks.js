ebbd252c44e3466f0b7461666f0dbb6ddfe2fa2e
joseph park <jpark041@gmail.com>
2016-02-03 11:22:55 -0800

    user add & auto update onclick works on db

--- server/controllers/parks.js
+++ server/controllers/parks.js
@@ -6,19 +6,19 @@ module.exports = (function() {
 		getUser: function(req, res){
 			console.log("Server / Ctrl / Users - Index")
 		},
-		findUser: function(req, res){
+		// findUser: function(req, res){
 
-			Park.findOne({users: {$elemMatch:{$eq:req.body.user_id}}}, function(err, parkInfo){
-				if(err){
-					res.json(err)
-				}else{
-					res.json(parkInfo)
-				}
-			})
-		},
+		// 	Park.findOne({users: {$elemMatch:{$eq:req.body.user_id}}}, function(err, parkInfo){
+		// 		if(err){
+		// 			res.json(err)
+		// 		}else{
+		// 			res.json(parkInfo)
+		// 		}
+		// 	})
+		// },
 		add: function(req, res){
 
-			console.log(req.body);
+			//console.log(req.body);
 
 			User.findOne({_id: req.body.user}, function(err, user){
 				if(!user.atPark){
@@ -26,9 +26,9 @@ module.exports = (function() {
 					user.save();
 
 					Park.findOne({google_id: req.body.park_id},function(err, park){
-						//console.log(park);
+						console.log(user);
 						if(park){
-							park.users.push(req.body.user_id);
+							park.users.push(user.alias);
 
 							park.save(function(err){
 								if(err){
@@ -41,7 +41,7 @@ module.exports = (function() {
 							newPark.name = req.body.name;
 							newPark.google_id = req.body.park_id;
 
-							newPark.users.push(user);
+							newPark.users.push(user.alias);
 
 							newPark.save(function(err){
 								if(err)
@@ -50,6 +50,40 @@ module.exports = (function() {
 									res.json({message: "added new park and user to park"});
 							})
 						}
+					})
+				}else{
+					
+					Park.findOne({users: {$elemMatch:{$eq:user.alias}}}, function(err, parkInfo){
+						console.log(parkInfo)
+						Park.findOneAndUpdate({"google_id" : parkInfo.google_id}, {$pull: {"users": user.alias}}, function(err, park){
+							// park.save();
+						})
+
+						Park.findOne({google_id: req.body.park_id},function(err, park){
+							//console.log(user);
+							if(park){
+								park.users.push(user.alias);
+								park.save(function(err){
+									if(err){
+										console.log(err);
+									}
+									res.json({message: "user added to park"});
+								})
+							} else {
+								var newPark = new Park();
+								newPark.name = req.body.name;
+								newPark.google_id = req.body.park_id;
+
+								newPark.users.push(user.alias);
+
+								newPark.save(function(err){
+									if(err)
+										res.json();
+									else
+										res.json({message: "added new park and user to park"});
+								})
+							}
+						})
 					})
 				}
 			})
--- server/models/park.js
+++ server/models/park.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var ParkSchema = new mongoose.Schema({
   name: { type: String, unique: true},
   google_id: {type: String},
-  users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true}],
+  users: [{type: String, unique: true}],
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now }
 });
